d6b8ed31453f171bd7f681c656738f3bd17c28fe
bramha-deshmukh17 <bramhadeshmukh3@gmail.com>
2025-04-22 15:07:19 +0530

    FIXED: connection to atlas

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+// MongoDB Atlas Connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
 
 app.get('/', (req, res) => {
     res.send('API is running');
